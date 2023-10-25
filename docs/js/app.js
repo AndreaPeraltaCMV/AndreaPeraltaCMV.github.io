032bcc989c31baffd42bd38408492df07cd50f45
MirellePeralta <a.mirelleperalta@gmail.com>
2023-10-25 16:22:09 -0600

    Uno

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,18 +1,75 @@
 const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
 
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
+formulario.addEventListener('submit', async(e) => {
+	e.preventDefault();
 
-    fetch('https://docs.google.com/spreadsheets/d/1084Oug8HH-9Cf_ecagMzhwe-TZWh78OqvnB_P6uzllM/edit?usp=sharing', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "Nombre": formulario.nombre.value,
-            "Correo": formulario.correo.value,
-            "Telefono": formulario.telefono.value
-        })
-    });
+// Escribir Filas
+try {
+	const respuesta = await fetch('https://sheet.best/api/sheets/d3f7e817-4130-4931-a597-c7638d7e0893', {
+		method: 'POST',
+	 		mode: 'cors',
+			headers: {
+	 			'Content-Type': 'application/json'
+		},
+	 		body: JSON.stringify({
+	 			"Nombre": formulario.nombre.value,
+	 			"Correo": formulario.correo.value,
+	 			"Telefono": formulario.telefono.value
+	 		})
+	 	});
+
+	 	const contenido = await respuesta.json();
+	 	console.log(contenido);
+	 } catch(error){
+	 	console.log(error);
+	 }
+
+ // Leer Filas
+	 try {
+	 	const respuesta = await fetch('https://sheet.best/api/sheets/e1bf0443-8bca-42a4-afc2-4f36b4e8fbd5');
+
+	 	const contenido = await respuesta.json();
+	 	console.log(contenido);
+	 } catch(error){
+	 	console.log(error);
+	 }
+
+
+// Eliminar Filas
+	 try {
+	 	const respuesta = await fetch('https://sheet.best/api/sheets/e1bf0443-8bca-42a4-afc2-4f36b4e8fbd5/0', {
+	 		method: 'DELETE'
+	 	});
+
+	 	const contenido = await respuesta.json();
+	 	console.log(contenido);
+	 } catch(error){
+	 	console.log(error);
+	 }
+
+
+// Actualizar Filas
+	try {
+		const respuesta = await fetch('https://sheet.best/api/sheets/e1bf0443-8bca-42a4-afc2-4f36b4e8fbd5/3', {
+			method: 'PATCH',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"Telefono": "000 000 000"
+			})
+		});
+
+		const contenido = await respuesta.json();
+		console.log(contenido);
+	} catch(error){
+		console.log(error);
+	}
+
+
+	 registro.classList.remove('activo');
+	 exito.classList.add('activo');
 });
